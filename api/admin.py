d60c18b19f5da9f95b59ac4597291673846ed4c3
Denzel <jdenzelmab@gmail.com>
2024-02-17 15:56:05 -0500

    test

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import TimeClock
 # Register your models here.
+
+class TimeClockAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'date', 'clock_in_time', 'clock_out_time', 'location', 'role')
+
+admin.site.register(TimeClock, TimeClockAdmin)
